054474bcbdbf38a9d6835f45edc1bc8f2b90d59f
Naresh Prajapati <nareshprajapati9313@gmail.com>
2025-12-15 14:29:51 +0530

    add tic tac toe

---
+++ Backend/index.js
@@ -0,0 +1,65 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: "http://localhost:5173",
+});
+
+const allUsers = {};
+
+io.on("connection", (socket) => {
+  // console.log(socket);
+  allUsers[socket.id] = {
+    socket: socket,
+    online: true,
+  };
+
+  socket.on("request_to_play", (data) => {
+    const currentUser = allUsers[socket.id];
+    currentUser.playerName = data.playerName;
+
+    let opponentPlayer;
+
+    for (const key in allUsers) {
+      const user = allUsers[key];
+      if (user.online && !user.playing && socket.id !== key) {
+        opponentPlayer = user;
+        break;
+      }
+    }
+    if (opponentPlayer) {
+      currentUser.socket.emit("OpponentFound", {
+        opponentName: opponentPlayer.playerName,
+        playingAs: "circle",
+      });
+      opponentPlayer.socket.emit("OpponentFound", {
+        opponentName: currentUser.playerName,
+        playingAs: "cross",
+      });
+      currentUser.socket.on("playerMoveFromClient", (data) => {
+        opponentPlayer.socket.emit("playerMoveFromServer", {
+        ...data,
+        });
+      });
+      opponentPlayer.socket.on("playerMoveFromClient", (data) => {
+        currentUser.socket.emit("playerMoveFromServer", {
+          ...data,
+        });
+      });
+    } else {
+      currentUser.socket.emit("OpponentNotFound");
+    }
+  });
+
+  socket.on("disconnect", function () {
+    // allUsers[socket.id] = {
+    //   socket: { ...socket, online: false },
+    //   online: true,
+    // };
+    const currentUser = allUsers[socket.id];
+    currentUser.online = false;
+  });
+});
+
+httpServer.listen(3000);
